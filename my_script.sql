8c362b67fde72571df409d84f5472b8e4389b372
mBentaifour <bentaifourmoh@gmail.com>
2024-06-27 15:33:01 -0700

    AirBnB_clone_v2

---
+++ my_script.sql
@@ -0,0 +1,3 @@
+-- first 3 students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT id 379080, Bentaifour Mohammed FROM Morocco students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
